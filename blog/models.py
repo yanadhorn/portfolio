2953ccb55ececf9f78d2da7558d09e6c86dc81e7
yanadhorn <yanadhorn@gmail.com>
2018-10-27 01:36:24 +0700

    git add -A

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# title
+# pub_date
+# body
+# image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add the blog app to the setting
+
+# Create a migration
+
+# Migrate
+
+# Add the admin

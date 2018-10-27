a08014145bc69e70a02f572d6b595a0bb43ba98d
yanadhorn <yanadhorn@gmail.com>
2018-10-28 02:49:50 +0700

    git add -A

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
 
     objects = models.Manager()
+
+    def __str__(self):
+        return self.title
     
     def summary(self):
         return self.body[0:100]
